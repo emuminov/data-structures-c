2d7d8653e45b17d4c243f7348e6d246b84d52f49
pdlmn <jseldardw@gmail.com>
2023-11-01 01:10:23 +0100

    feat: add deletion of items

--- hash_table.c
+++ hash_table.c
@@ -1,10 +1,11 @@
-#include <stdlib.h>
-// TODO: replace with my own library functions
+#include <stdlib.h> // TODO: replace with my own library functions
 #include <string.h>
 #include <math.h>
 
 #include "base.h"
 #include "hash_table.h"
+
+local ht_item HT_DELETED_ITEM = {0, 0};
 
 local ht_item *ht_new_item(const char *k, const char *v) {
     ht_item *i = malloc(sizeof(ht_item));
@@ -41,13 +42,7 @@ void ht_del_hash_table(ht_hash_table *ht) {
     free(ht);
 }
 
-/* function hash(string, a, num_buckets):
-    hash = 0
-    string_len = length(string)
-    for i = 0, 1, ..., string_len:
-        hash += (a ** (string_len - (i+1))) * char_code(string[i])
-    hash = hash % num_buckets
-    return hash */
+// bash hash function
 local int ht_hash(const char *s, const int a, const int m) {
     long hash = 0;
     // TODO: replace with my strlen function
@@ -62,8 +57,52 @@ local int ht_hash(const char *s, const int a, const int m) {
     return (int) hash;
 }
 
+// actual hash function that double hashes
 local int ht_get_hash(const char *s, const int buckets, const int attempt) {
     const int hash_a = ht_hash(s, HT_PRIME_1, buckets);
     const int hash_b = ht_hash(s, HT_PRIME_2, buckets);
     return (hash_a + (attempt * (hash_b + 1)) % buckets);
+}
+
+void ht_insert(ht_hash_table *ht, const char *key, const char *value) {
+    ht_item *new_item = ht_new_item(key, value);
+    int attempt = 0;
+    int index = ht_get_hash(key, ht->size, attempt);
+    ht_item *curr_item = ht->items[index];
+    while (curr_item != 0 && curr_item != &HT_DELETED_ITEM) {
+        index = ht_get_hash(new_item->key, ht->size, ++attempt);
+        curr_item = ht->items[index];
+    }
+    ht->items[index] = new_item;
+    ht->count++;
+}
+
+char *ht_search(ht_hash_table *ht, const char *key) {
+    int attempt = 0;
+    int index = ht_get_hash(key, ht->size, attempt);
+    ht_item *item = ht->items[index];
+    while (item != 0) {
+        // TODO: replace with my own function
+        if (strcmp(key, item->key) == 0 && item != &HT_DELETED_ITEM) {
+            return item->value;
+        }
+        index = ht_get_hash(key, ht->size, ++attempt);
+        item = ht->items[index];
+    }
+    return (0);
+}
+
+void ht_delete(ht_hash_table *ht, const char *key) {
+    int attempt = 0;
+    int index = ht_get_hash(key, ht->size, attempt);
+    ht_item *item = ht->items[index];
+    while (item != 0) {
+        if (item != &HT_DELETED_ITEM && strcmp(key, item->key) == 0) {
+            ht_del_item(item);
+            ht->items[index] = &HT_DELETED_ITEM;
+        }
+        index = ht_get_hash(key, ht->size, ++attempt);
+        item = ht->items[index];
+    }
+    ht->count--;
 }
