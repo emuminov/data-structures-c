9dc132b1d8d2f240ea7af4b07852c68adfba76e2
pdlmn <jseldardw@gmail.com>
2023-11-02 01:32:16 +0100

    feat: added new macro constant and updated functions

--- hash_table.h
+++ hash_table.h
@@ -4,6 +4,7 @@ typedef struct {
 } ht_item;
 
 typedef struct {
+    int base_size;
     int size;
     int count;
     ht_item **items;
@@ -11,6 +12,10 @@ typedef struct {
 
 ht_hash_table *ht_new(void);
 void ht_del_hash_table(ht_hash_table *ht);
+void ht_insert(ht_hash_table *ht, const char *key, const char *value);
+char *ht_search(ht_hash_table *ht, const char *key);
+void ht_delete(ht_hash_table *ht, const char *key);
 
 #define HT_PRIME_1 151
 #define HT_PRIME_2 163
+#define HT_INITIAL_BASE_SIZE 53
